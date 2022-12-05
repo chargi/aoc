9f96ded3efdb749ca2d5cb1e710e3c2568741126
Chargi <chargi@chargi.at>
2022-12-05 20:25:03 +0100

    Refactored Day 3

--- 3/main.py
+++ 3/main.py
@@ -8,63 +8,36 @@ items = {}
 def scoring(char):
     #ascii A=65, Z=90 - a=97, z=122
     value = ord(char)
-    #a-z
-    if value >= 97 and value <= 122:
+    if char.islower():
         return(value-96)
-    #A-Z
-    if value >= 65 and value <= 90:
+    else:
         return(value-38)
 
 #Read lines and put half of items in each compartment
 def part1(rucksack):
     score = 0
-
     for line in rucksack:
         half_length = len(line)//2
         left = line[:half_length]
         right = line[half_length:]
         #Find first item that is also found in the other compartment, cancel search and add priority score
-        found = False
         for l in left:
-            if found:
+            if l in right:
+                score += scoring(l)
                 break
-            for r in right:
-                if l == r:
-                    found = True
-                    score += scoring(l)
-                    break
     return score
 
 #not beautiful but gets the job done
 def part2(rucksack):
-    
     score = 0
-    
-    while rucksack:
-        group = []
-        found = False
-
-        i = 0
-        while i <= 2:
-            if rucksack:
-                group.append(rucksack.pop())
-                i=i+1
-
-        if not group:
-            break
-
-        for c1 in group[0]:
-            if found:
+    for i in range(0, len(rucksack), 3):
+        line1 = rucksack[i]
+        line2 = rucksack[i+1]
+        line3 = rucksack[i+2]
+        for c in line1:
+            if c in line2 and c in line3:
+                score += scoring(c)
                 break
-            for c2 in group[1]:
-                if found:
-                    break
-                if c1==c2:
-                    for c3 in group[2]:
-                        if c1==c3:
-                            score += scoring(c1)
-                            found = True
-                            break
     return score
     
 
